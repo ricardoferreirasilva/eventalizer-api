0bf6dd306e35cc08052541cacb2beab3a6d30e69
Ricardo Ferreira da Silva <tansvaino@hotmail.com>
2019-11-16 10:42:37 +0000

    Created users module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 import * as Environments from "dotenv";
 Environments.config();
-
 @Module({
-  imports: [MongooseModule.forRoot(process.env.db_url,{useUnifiedTopology:true,useNewUrlParser:true})],
+  imports: [MongooseModule.forRoot(process.env.db_url,{useUnifiedTopology:true,useNewUrlParser:true}), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
